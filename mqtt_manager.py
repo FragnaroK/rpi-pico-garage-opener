fd2dcd0afc3eac9c48b19eea8b734e41d0b6ebce
FragnaroK <fcanalejo2000@outlook.com>
2026-06-01 00:38:16 +1000

    Refactor MQTTManager to use constants for status topics and improve connection checks; remove obsolete test file.

--- mqtt_manager.py
+++ mqtt_manager.py
@@ -2,6 +2,9 @@ import time
 import gc
 from umqtt.simple import MQTTClient
 
+DEFAULT_STATUS_SUFFIX = b'/status'
+ERROR_NOT_CONNECTED = 'Not connected'
+
 
 class MQTTManager:
     """Small wrapper around umqtt.simple.MQTTClient to centralize reconnect
@@ -19,11 +22,12 @@ class MQTTManager:
         self.ssl = ssl
         self.ssl_params = ssl_params or {}
         self.topic_base = topic_base
-        self.lw_topic = lw_topic or (topic_base + b'/status')
+        self.lw_topic = lw_topic or (topic_base + DEFAULT_STATUS_SUFFIX)
         self.lw_msg = lw_msg
 
         self.client = None
         self.cb = None
+        self.last_ping = 0
 
     def set_callback(self, cb):
         self.cb = cb
@@ -67,9 +71,10 @@ class MQTTManager:
                 pass
             # publish online status
             try:
-                self.client.publish(self.topic_base + b'/status', b'ONLINE', retain=True)
+                self.client.publish(self.topic_base + DEFAULT_STATUS_SUFFIX, b'ONLINE', retain=True)
             except Exception:
                 pass
+            self.last_ping = time.time()
             return True
         except Exception:
             return False
@@ -78,7 +83,7 @@ class MQTTManager:
         try:
             if self.client:
                 try:
-                    self.client.publish(self.topic_base + b'/status', b'OFFLINE', retain=True)
+                    self.client.publish(self.topic_base + DEFAULT_STATUS_SUFFIX, b'OFFLINE', retain=True)
                 except Exception:
                     pass
                 try:
@@ -87,6 +92,7 @@ class MQTTManager:
                     pass
         finally:
             self.client = None
+            self.last_ping = 0
             gc.collect()
 
     def publish(self, topic, msg, retain=False, qos=0):
@@ -94,10 +100,26 @@ class MQTTManager:
             raise OSError('Not connected')
         return self.client.publish(topic, msg, retain=retain, qos=qos)
 
+    def _send_keepalive_ping(self):
+        if not self.keepalive or not self.client:
+            return
+        now = time.time()
+        if now - self.last_ping >= self.keepalive / 2:
+            self.client.ping()
+            self.last_ping = now
+
     def check_msg(self):
-        if not self.client:
-            raise OSError('Not connected')
-        return self.client.check_msg()
+        if not self.client or not getattr(self.client, 'sock', None):
+            raise OSError(ERROR_NOT_CONNECTED)
+        sock = self.client.sock
+        if sock is None:
+            raise OSError(ERROR_NOT_CONNECTED)
+        sock.setblocking(False)
+        try:
+            result = self.client.wait_msg()
+            return result
+        finally:
+            self._send_keepalive_ping()
 
     def is_connected(self):
-        return self.client is not None
+        return self.client is not None and getattr(self.client, 'sock', None) is not None
